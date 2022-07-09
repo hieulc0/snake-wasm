1b7cb9a842e541005f216474e35a56ee36e5bb35
HieuLC <leconghieu1998@gmail.com>
2022-07-09 18:54:19 +0700

    canvas cannot work. and console.log

--- src/lib.rs
+++ src/lib.rs
@@ -8,11 +8,6 @@ extern {
 }
 
 #[wasm_bindgen]
-pub fn greet(s: &str) {
-    alert(s);
-}
-
-#[wasm_bindgen]
 extern "C" {
     #[wasm_bindgen(js_namespace = console)]
     fn log(s: &str);
@@ -22,47 +17,55 @@ macro_rules! console_log {
     ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
 }
 
-#[wasm_bindgen(start)]
-pub fn start() {
-    let document = web_sys::window().unwrap().document().unwrap();
-    let canvas = document.get_element_by_id("canvas").unwrap();
-    let canvas: web_sys::HtmlCanvasElement = canvas
-        .dyn_into::<web_sys::HtmlCanvasElement>()
-        .map_err(|_| ())
-        .unwrap();
-
-    let context = canvas
-        .get_context("2d")
-        .unwrap()
-        .unwrap()
-        .dyn_into::<web_sys::CanvasRenderingContext2d>()
-        .unwrap();
-
-    context.begin_path();
-    
-    // Draw the outer circle.
-    context
-        .arc(75.0, 75.0, 50.0, 0.0, std::f64::consts::PI * 2.0)
-        .unwrap();
-
-    // Draw the mouth.
-    context.move_to(110.0, 75.0);
-    context.arc(75.0, 75.0, 35.0, 0.0, std::f64::consts::PI).unwrap();
-
-    // Draw the left eye.
-    context.move_to(65.0, 65.0);
-    context
-        .arc(60.0, 65.0, 5.0, 0.0, std::f64::consts::PI * 2.0)
-        .unwrap();
-
-    // Draw the right eye.
-    context.move_to(95.0, 65.0);
-    context
-        .arc(90.0, 65.0, 5.0, 0.0, std::f64::consts::PI * 2.0)
-        .unwrap();
-
-    context.stroke();
-
+#[wasm_bindgen]
+pub fn greet(s: &str) {
+    console_log!("{}", s);
+    alert(s);
 }
+
+
+
+//#[wasm_bindgen(start)]
+//pub fn start() {
+//    let document = web_sys::window().unwrap().document().unwrap();
+//    let canvas = document.get_element_by_id("canvas").unwrap();
+//    let canvas: web_sys::HtmlCanvasElement = canvas
+//        .dyn_into::<web_sys::HtmlCanvasElement>()
+//        .map_err(|_| ())
+//        .unwrap();
+//
+//    let context = canvas
+//        .get_context("2d")
+//        .unwrap()
+//        .unwrap()
+//        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+//        .unwrap();
+//
+//    context.begin_path();
+//    
+//    // Draw the outer circle.
+//    context
+//        .arc(75.0, 75.0, 50.0, 0.0, std::f64::consts::PI * 2.0)
+//        .unwrap();
+//
+//    // Draw the mouth.
+//    context.move_to(110.0, 75.0);
+//    context.arc(75.0, 75.0, 35.0, 0.0, std::f64::consts::PI).unwrap();
+//
+//    // Draw the left eye.
+//    context.move_to(65.0, 65.0);
+//    context
+//        .arc(60.0, 65.0, 5.0, 0.0, std::f64::consts::PI * 2.0)
+//        .unwrap();
+//
+//    // Draw the right eye.
+//    context.move_to(95.0, 65.0);
+//    context
+//        .arc(90.0, 65.0, 5.0, 0.0, std::f64::consts::PI * 2.0)
+//        .unwrap();
+//
+//    context.stroke();
+//
+//}
 
 

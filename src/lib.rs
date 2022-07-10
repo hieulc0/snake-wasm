75d6b1fcc05ff214ab27516e8ee08a629d14e271
HieuLC <leconghieu1998@gmail.com>
2022-07-10 22:20:47 +0700

    create cavas

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 
-
 #[wasm_bindgen]
 extern {
     pub fn alert(s: &str);
@@ -34,55 +33,21 @@ pub fn start() -> web_sys::Element {
     let window = web_sys::window().expect("no global `window` exists");
     let document = window.document().expect("should have a document on window");
 
-    let canvas = document.create_element("div").expect("fail to create new element");
-    canvas.set_text_content(Some("this is content of canvas"));
-    console_log!("rust: create canvas: {:?}", canvas);
+    let canvas_container = document.create_element("div").expect("fail to create new element");
+    canvas_container.set_text_content(Some("this is content of canvas"));
+    console_log!("rust: create canvas: {:?}", canvas_container);
 
-//let c = document
-//    .create_element("div")
-//    .expect("fail to create new element")
-//    .dyn_into::<web_sys::HtmlCanvasElement>()
-//    .map_err(|_| ())
-//    .unwrap();
+    let canvas = document
+        .create_element("canvas")
+        .expect("fail to create new element")
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .map_err(|_| ())
+        .expect("fail to cast canvas to canvas element");
+    console_log!("create another div: {:?}", canvas);
 
-    // console_log!("rust: dyn_into: {:?}", c);
-    return canvas;
-
-
-    
      
-// let context = canvas
-//     .get_context("2d")
-//     .unwrap()
-//     .unwrap()
-//     .dyn_into::<web_sys::CanvasRenderingContext2d>()
-//     .unwrap();
-// 
-// context.begin_path();
-// 
-// // Draw the outer circle.
-// context
-//     .arc(75.0, 75.0, 50.0, 0.0, std::f64::consts::PI * 2.0)
-//     .unwrap();
-// 
-// // Draw the mouth.
-// context.move_to(110.0, 75.0);
-// context.arc(75.0, 75.0, 35.0, 0.0, std::f64::consts::PI).unwrap();
-// 
-// // Draw the left eye.
-// context.move_to(65.0, 65.0);
-// context
-//     .arc(60.0, 65.0, 5.0, 0.0, std::f64::consts::PI * 2.0)
-//     .unwrap();
-// 
-// // Draw the right eye.
-// context.move_to(95.0, 65.0);
-// context
-//     .arc(90.0, 65.0, 5.0, 0.0, std::f64::consts::PI * 2.0)
-//     .unwrap();
-// 
-// context.stroke();
-
+    
+    return canvas_container;
 }
 
 

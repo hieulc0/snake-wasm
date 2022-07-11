2f05f76b20045d140f95a0dadb05a37ef7dc671f
HieuLC <leconghieu1998@gmail.com>
2022-07-11 22:36:26 +0700

    draw app in canvas

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+use std::cell::Cell;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 
@@ -43,6 +45,10 @@ pub fn start() -> web_sys::Element {
         .dyn_into::<web_sys::HtmlCanvasElement>()
         .map_err(|_| ())
         .expect("fail to cast canvas to canvas element");
+
+    canvas.set_width(640);
+    canvas.set_height(480);
+    canvas.style().set_property("border", "solid").expect("fail to set border");
     console_log!("create canvas: {:?}", canvas);
 
     let context = canvas
@@ -50,32 +56,46 @@ pub fn start() -> web_sys::Element {
         .unwrap()
         .unwrap()
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
-        .unwrap();
-    console_log!("create context from canvas: {:?}", context);
+        .expect("fail to dyn cast to canvas");
 
-    // Draw the outer circle.
-    context
-        .arc(75.0, 75.0, 50.0, 0.0, std::f64::consts::PI * 2.0)
-        .unwrap();
-
-    // Draw the mouth.
-    context.move_to(110.0, 75.0);
-    context.arc(75.0, 75.0, 35.0, 0.0, std::f64::consts::PI).unwrap();
-
-    // Draw the left eye.
-    context.move_to(65.0, 65.0);
-    context
-        .arc(60.0, 65.0, 5.0, 0.0, std::f64::consts::PI * 2.0)
-        .unwrap();
-
-    // Draw the right eye.
-    context.move_to(95.0, 65.0);
-    context
-        .arc(90.0, 65.0, 5.0, 0.0, std::f64::consts::PI * 2.0)
-        .unwrap();
-
-    context.stroke();
-
+    let context = Rc::new(context);
+    let pressed = Rc::new(Cell::new(false));
+    {
+        let context = context.clone();
+        let pressed = pressed.clone();
+        let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
+            context.begin_path();
+            context.move_to(event.offset_x() as f64, event.offset_y() as f64);
+            pressed.set(true);
+        }) as Box<dyn FnMut(_)>);
+        canvas.add_event_listener_with_callback("mousedown", closure.as_ref().unchecked_ref()).expect("fail to add listener mousedown");
+        closure.forget();
+    }
+    {
+        let context = context.clone();
+        let pressed = pressed.clone();
+        let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
+            if pressed.get() {
+                context.line_to(event.offset_x() as f64, event.offset_y() as f64);
+                context.stroke();
+                context.begin_path();
+                context.move_to(event.offset_x() as f64, event.offset_y() as f64);
+            }
+        }) as Box<dyn FnMut(_)>);
+        canvas.add_event_listener_with_callback("mousemove", closure.as_ref().unchecked_ref()).expect("fail to add listener mousemove");
+        closure.forget();
+    }
+    {
+        let context = context.clone();
+        let pressed = pressed.clone();
+        let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
+            pressed.set(false);
+            context.line_to(event.offset_x() as f64, event.offset_y() as f64);
+            context.stroke();
+        }) as Box<dyn FnMut(_)>);
+        canvas.add_event_listener_with_callback("mouseup", closure.as_ref().unchecked_ref()).expect("fail to add listener to mouseup");
+        closure.forget();
+    }
     canvas_container.append_child(&canvas).expect("fail to append child canvas");
     return canvas_container;
 }
